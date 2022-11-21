fcd2af2b26e94c9e21786ec38bcd5040fab355b2
sean-code <john.nganga@student.moringaschool.com>
2022-11-21 18:01:24 +0300

    Add bous deliverable

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,11 @@ class StudentsController < ApplicationController
         all_students = Student.order(grade: :desc)
         render json: all_students
     end
+    # Bonus Lab
+    def high_grade
+       
+        students = Student.maximum(:grade)
+        student_x =  Student.select("Max(grade) as max_grade").first.attributes
+        render json: students
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
  get "students", to:"students#index"
 
  get "students/grades", to:"students#grades"
+
+#  Bonus lab
+ get "/students/high-grade", to:"students#high_grade"
 end
